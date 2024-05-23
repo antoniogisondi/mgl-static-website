31d22771944ed471fccc9d42a53afbd90242fb01
Antonio <anto.gisondi@tiscali.it>
2024-05-23 11:41:47 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,7 @@ import ContactUs from "./pages/ContactUs/ContactUs";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 // index.js or App.js
-import $ from 'jquery';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <>
