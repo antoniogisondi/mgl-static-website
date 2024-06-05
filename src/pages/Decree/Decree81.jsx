26400d4c4ea71642dfd3f8c899bb3bcc28e76544
Antonio <anto.gisondi@tiscali.it>
2024-06-05 11:29:29 +0200

    add text to d.lgs 81

--- src/pages/Decree/Decree81.jsx
+++ src/pages/Decree/Decree81.jsx
@@ -10,7 +10,149 @@ const Decree81 = () => {
             <main className='container-fluid'>
                 <div className="row">
                     <div className="col">
-                        <h3>Qua vanno inseriti la legge del decreto</h3>
+                        <h3>Introduzione al D.Lgs 81/2008</h3>
+                        <p>Il Decreto Legislativo 81/2008, noto anche come Testo Unico sulla Salute e Sicurezza sul Lavoro, rappresenta un pilastro fondamentale per la tutela dei lavoratori. Esso stabilisce gli obblighi dei datori di lavoro e i diritti dei dipendenti al fine di garantire ambienti di lavoro sicuri e salubri. Nel seguito, esploreremo le principali disposizioni del D.Lgs 81/2008 e le responsabilità delle diverse figure coinvolte.</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <h3>Figure Coinvolte</h3>
+                        <div>
+                            <p>Il Decreto Legislativo 81/08 regola la sicurezza sul lavoro in Italia e coinvolge diverse figure professionali. Ecco una breve descrizione di alcune di queste figure:</p>
+                        </div>
+                        <ul>
+                            <li>
+                                Datore di lavoro: È il titolare del rapporto di lavoro con il lavoratore e ha la responsabilità dell’organizzazione o dell’unità produttiva. Deve valutare i rischi nel luogo di lavoro, fornire dispositivi di protezione ai lavoratori e garantire la formazione adeguata.
+                            </li>
+                            <li>
+                                Dirigente: Attua le direttive del datore di lavoro, organizzando le attività lavorative e vigilando su di esse. Ha gli stessi obblighi dei datori di lavoro.
+                            </li>
+                            <li>
+                                Responsabile del servizio di prevenzione e protezione (RSPP): Coordinatore del servizio di prevenzione e protezione dai rischi, designato dal datore di lavoro. Deve possedere capacità adeguate alla natura dei rischi presenti nel luogo di lavoro.
+                            </li>
+                            <li>
+                                Addetto al servizio di prevenzione e protezione (ASPP): Collaboratore del RSPP, designato quando necessario dal datore di lavoro. Devono essere presenti in un numero sufficiente in base alle caratteristiche dell’impresa.
+                            </li>
+                            <li>
+                                Preposto: Sovrintende all’attività lavorativa e garantisce l’attuazione delle direttive ricevute. Deve frequentare corsi di formazione adeguati.
+                            </li>
+                            <li>
+                                Rappresentante dei lavoratori per la sicurezza (RLS): Persona eletta o designata per rappresentare i lavoratori riguardo alla salute e sicurezza durante il lavoro
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <h3>Valutazione dei rischi e DVR</h3>
+                        <div>
+                            <h4>Valutazione dei Rischi</h4>
+                            <p>
+                                La valutazione dei rischi è un processo continuo che coinvolge l’identificazione, l’analisi e la gestione dei potenziali pericoli presenti nell’ambiente di lavoro.
+                            </p>
+                            <div>
+                                <ul>
+                                    <li>
+                                        Perché è importante? La valutazione dei rischi permette di:
+                                        <ul>
+                                            <li>
+                                                Prevenire incidenti e malattie professionali.
+                                            </li>
+                                            <li>
+                                                Proteggere la salute e la sicurezza dei lavoratori.
+                                            </li>
+                                            <li>
+                                                Rispettare gli obblighi legali e normativi.
+                                            </li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+                                <ul>
+                                    <li>
+                                        Come si svolge la valutazione dei rischi?
+                                        <ul>
+                                            <li>
+                                                Identificazione dei pericoli: Si individuano le situazioni o le condizioni che potrebbero causare danni.
+                                            </li>
+                                            <li>
+                                                Valutazione dei rischi: Si analizza la probabilità di occorrenza di tali pericoli e il loro impatto sulla salute e la sicurezza.
+                                            </li>
+                                            <li>
+                                                Definizione di misure preventive: Si stabiliscono azioni per ridurre o eliminare i rischi.
+                                            </li>
+                                            <li>
+                                                Monitoraggio e aggiornamento: La valutazione dei rischi deve essere rivista periodicamente.
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        Gli strumenti utilizzati includono check-list, analisi SWOT, diagrammi di flusso e altre metodologie specifiche.
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div>
+                            <h4>Documento di Valutazione dei Rischi (DVR)</h4>
+                            <p>
+                                Il DVR è un documento scritto che raccoglie tutte le informazioni relative ai rischi presenti nell’azienda.
+                            </p>
+                            <div>
+                                <ul>
+                                    <li>
+                                        Contenuto del DVR:
+                                        <ul>
+                                            <li>
+                                                Anagrafica dell’azienda: Ragione sociale, indirizzo, settore di attività.
+                                            </li>
+                                            <li>
+                                                Descrizione degli ambienti di lavoro: Specifica delle aree, macchinari, attrezzature.
+                                            </li>
+                                            <li>
+                                                Analisi dei rischi: Identificazione e valutazione dei rischi specifici per ciascuna attività.
+                                            </li>
+                                            <li>
+                                                Misure preventive e di protezione: Indicazione delle azioni da adottare per ridurre o eliminare i rischi.
+                                            </li>
+                                            <li>
+                                                Elenco dei lavoratori esposti a rischi: Nomi e mansioni.
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        Il DVR deve essere redatto in modo chiaro e comprensibile, coinvolgendo i lavoratori e i rappresentanti sindacali.
+                                    </li>
+                                    <li>
+                                        È un documento dinamico: va aggiornato in caso di modifiche nell’organizzazione o nei rischi presenti.
+                                    </li>
+                                </ul>
+                            </div>
+                            <div>
+                                <ul>
+                                    <li>
+                                        Come si svolge la valutazione dei rischi?
+                                        <ul>
+                                            <li>
+                                                Identificazione dei pericoli: Si individuano le situazioni o le condizioni che potrebbero causare danni.
+                                            </li>
+                                            <li>
+                                                Valutazione dei rischi: Si analizza la probabilità di occorrenza di tali pericoli e il loro impatto sulla salute e la sicurezza.
+                                            </li>
+                                            <li>
+                                                Definizione di misure preventive: Si stabiliscono azioni per ridurre o eliminare i rischi.
+                                            </li>
+                                            <li>
+                                                Monitoraggio e aggiornamento: La valutazione dei rischi deve essere rivista periodicamente.
+                                            </li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        Gli strumenti utilizzati includono check-list, analisi SWOT, diagrammi di flusso e altre metodologie specifiche.
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </main>
